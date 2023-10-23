4cfe9602acceb36ac743706a3f639f7dd7766b4c
v1xingyue <qixingyue@gmail.com>
2023-10-23 14:24:47 +0800

    publish devnet

--- src/chain/config.ts
+++ src/chain/config.ts
@@ -1,9 +1,9 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
-const NETWORK: NetworkType = 'localnet';
+const NETWORK: NetworkType = 'devnet';
 
-const PACKAGE_ID = '0x33c0b6f6ae204ae2539ca6af9774c44ceffc8bb47553b38670a5a180a815df71'
-const WORLD_ID = '0x0e5b15193e391d76477f5940522e5f3cd0febb6109ffb092c276041e8f48aefb'
+const PACKAGE_ID = '0x5c8929dbea8f9b45f54ab42821766eb81f19d2dd785e3bff0abd06420633768b'
+const WORLD_ID = '0xf9c03517533207843f9ecea5b861cb2ef6aae13c6817cb8992576d60e2fe9bd5'
 
 export {
     NETWORK,
